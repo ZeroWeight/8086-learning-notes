2c8fcf1e2205270a33faffb27e55f59d62dd0bb9
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2017-03-19 20:09:56 +0800

    exp 1 powered by kingium

---
+++ assembly/p12.asm
@@ -0,0 +1,36 @@
+name my_program
+
+data segment
+buffer1 db 0,1,2,3,4,5,6,7,8,9,0ah,0bh,0ch,0dh,0eh,0fh
+buffer2 db 10h dup(0)
+mess db 'HAVE DONE',13,10,'$'
+data ends
+
+stack segment para stack
+db 100 dup(?)
+stack ends
+
+code segment
+assume cs:code, ds:data, es:data, ss:stack
+
+start: mov ax, data
+mov ds, ax
+mov es, ax
+lea si, buffer1
+lea di, buffer2
+mov cx, 10h
+next: mov al, [si]
+mov [di], al
+inc si
+inc di
+dec cx
+jnz next
+lea dx, mess
+mov ah, 9
+int 21h
+mov ah, 4ch
+int 21h
+
+code ends
+
+end start
---
+++ assembly/readme.txt
@@ -0,0 +1,11 @@
+建议将这个assembly文件夹拷贝到U盘，这样可以同时在自己电脑和实验室机器上工作。
+DOSBOX安装后，键入以下命令：
+
+mount C [assembly文件夹的路径，如E:\assembly] （如果有空格，需要把该名称用双引号括出）
+cd C:\
+
+之后C:\就是你在DOSBOX的虚拟工作目录了，这个还外界系统是隔离的。
+
+good luck & have fun
+
+P12是一个示例程序
